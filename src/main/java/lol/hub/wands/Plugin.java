3d91c803cf0755a4f61655de9021eee2c5a1a27f
Florian Hübner <code@hub.lol>
2024-02-02 22:22:26 +0100

    init

---
+++ src/main/java/lol/hub/wands/Plugin.java
@@ -0,0 +1,12 @@
+package lol.hub.wands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, World!");
+    }
+
+}
